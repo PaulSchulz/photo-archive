722a1eb3f280ff52de0e74044c945c16c0960193
Paul Schulz <paul@mawsonlakes.org>
2026-06-02 09:00:50 +0930

    Add gallery component

---
+++ src/photo_archive/components/gallery.cljs
@@ -0,0 +1,16 @@
+(ns photo-archive.components.gallery
+    (:require [reagent.core :as reagent]))
+
+(defn gallery-item [{:keys [id thumbnail title]} on-click]
+  [:div.gallery-item {:key id
+                      :on-click #(on-click id)}
+   [:img {:src thumbnail
+          :alt title}]
+   [:div.gallery-item-title title]])
+
+(defn gallery [photos on-photo-select]
+  [:div.gallery-container
+   [:div.gallery-grid
+    (doall (map (fn [photo]
+                  (gallery-item photo on-photo-select))
+                photos))]])
