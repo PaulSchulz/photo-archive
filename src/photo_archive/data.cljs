91eb60642072a4c1b3398f130aad0a70e9dd37a1
Paul Schulz <paul@mawsonlakes.org>
2026-06-02 09:15:30 +0930

    Replace placeholder images with SVG data URLs that work offline

--- src/photo_archive/data.cljs
+++ src/photo_archive/data.cljs
@@ -1,12 +1,21 @@
 (ns photo-archive.data)
 
+;; SVG placeholder generator
+(defn make-svg-placeholder [color text width height]
+  (str "data:image/svg+xml;utf8,"
+       "<svg xmlns='http://www.w3.org/2000/svg' width='" width "' height='" height "'>"
+       "<rect fill='" color "' width='" width "' height='" height "'/>"
+       "<text x='50%' y='50%' font-size='14' fill='white' text-anchor='middle' dy='.3em'>"
+       text "</text>"
+       "</svg>"))
+
 ;; Sample photo data - in production this would come from a backend/API
 (def sample-photos
   [{:id 1
     :title "Sunset at the Beach"
     :filename "sunset-beach.jpg"
-    :url "https://via.placeholder.com/300x200?text=Sunset"
-    :thumbnail "https://via.placeholder.com/150x100?text=Sunset"
+    :url (make-svg-placeholder "#ff6b35" "Sunset Beach" 300 200)
+    :thumbnail (make-svg-placeholder "#ff6b35" "Sunset" 150 100)
     :metadata {:date "2026-01-15"
                :camera "Canon EOS 5D"
                :iso 400
@@ -18,8 +27,8 @@
    {:id 2
     :title "Mountain Vista"
     :filename "mountain.jpg"
-    :url "https://via.placeholder.com/300x200?text=Mountain"
-    :thumbnail "https://via.placeholder.com/150x100?text=Mountain"
+    :url (make-svg-placeholder "#004e89" "Mountain Vista" 300 200)
+    :thumbnail (make-svg-placeholder "#004e89" "Mountain" 150 100)
     :metadata {:date "2026-02-03"
                :camera "Sony A7III"
                :iso 100
@@ -31,8 +40,8 @@
    {:id 3
     :title "City Lights"
     :filename "city-lights.jpg"
-    :url "https://via.placeholder.com/300x200?text=City"
-    :thumbnail "https://via.placeholder.com/150x100?text=City"
+    :url (make-svg-placeholder "#1b1e23" "City Lights" 300 200)
+    :thumbnail (make-svg-placeholder "#1b1e23" "City" 150 100)
     :metadata {:date "2026-01-28"
                :camera "Nikon Z6"
                :iso 3200
@@ -44,8 +53,8 @@
    {:id 4
     :title "Forest Path"
     :filename "forest.jpg"
-    :url "https://via.placeholder.com/300x200?text=Forest"
-    :thumbnail "https://via.placeholder.com/150x100?text=Forest"
+    :url (make-svg-placeholder "#2d6a4f" "Forest Path" 300 200)
+    :thumbnail (make-svg-placeholder "#2d6a4f" "Forest" 150 100)
     :metadata {:date "2026-03-10"
                :camera "Canon EOS 5D"
                :iso 200
@@ -57,8 +66,8 @@
    {:id 5
     :title "Ocean Waves"
     :filename "waves.jpg"
-    :url "https://via.placeholder.com/300x200?text=Waves"
-    :thumbnail "https://via.placeholder.com/150x100?text=Waves"
+    :url (make-svg-placeholder "#0077b6" "Ocean Waves" 300 200)
+    :thumbnail (make-svg-placeholder "#0077b6" "Waves" 150 100)
     :metadata {:date "2026-02-14"
                :camera "GoPro Hero 10"
                :iso 100
@@ -70,8 +79,8 @@
    {:id 6
     :title "Autumn Leaves"
     :filename "autumn.jpg"
-    :url "https://via.placeholder.com/300x200?text=Autumn"
-    :thumbnail "https://via.placeholder.com/150x100?text=Autumn"
+    :url (make-svg-placeholder "#d4a574" "Autumn Leaves" 300 200)
+    :thumbnail (make-svg-placeholder "#d4a574" "Autumn" 150 100)
     :metadata {:date "2026-03-25"
                :camera "Fujifilm X-T4"
                :iso 400
