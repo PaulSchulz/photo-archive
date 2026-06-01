8af3816aff65ca22714ef963b5ff3b5c57eeb6a0
Paul Schulz <paul@mawsonlakes.org>
2026-06-02 09:01:53 +0930

    Update core.cljs to integrate gallery and viewer components

--- src/photo_archive/core.cljs
+++ src/photo_archive/core.cljs
@@ -1,23 +1,63 @@
 (ns photo-archive.core
     (:require
               [reagent.core :as reagent :refer [atom]]
-              [reagent.dom :as rd]))
+              [reagent.dom :as rd]
+              [photo-archive.data :refer [sample-photos]]
+              [photo-archive.components.gallery :refer [gallery]]
+              [photo-archive.components.viewer :refer [photo-viewer]]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/photo-archive/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:photos sample-photos
+                          :selected-photo-id nil}))
 
+(defn get-photo-by-id [photos id]
+  (first (filter #(= (:id %) id) photos)))
 
-(defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+(defn get-photo-index [photos id]
+  (.indexOf (map :id photos) id))
 
-(rd/render [hello-world]
+(defn select-photo [id]
+  (swap! app-state assoc :selected-photo-id id))
+
+(defn close-viewer []
+  (swap! app-state assoc :selected-photo-id nil))
+
+(defn next-photo []
+  (let [current-id (:selected-photo-id @app-state)
+        photos (:photos @app-state)
+        current-index (get-photo-index photos current-id)
+        next-index (if (< current-index (- (count photos) 1))
+                     (+ current-index 1)
+                     0)
+        next-photo (nth photos next-index)]
+    (select-photo (:id next-photo))))
+
+(defn prev-photo []
+  (let [current-id (:selected-photo-id @app-state)
+        photos (:photos @app-state)
+        current-index (get-photo-index photos current-id)
+        prev-index (if (> current-index 0)
+                     (- current-index 1)
+                     (- (count photos) 1))
+        prev-photo (nth photos prev-index)]
+    (select-photo (:id prev-photo))))
+
+(defn app []
+  (let [photos (:photos @app-state)
+        selected-id (:selected-photo-id @app-state)
+        selected-photo (when selected-id
+                        (get-photo-by-id photos selected-id))]
+    [:div.app
+     [:header.app-header
+      [:h1 "Photo Archive"]]
+     [gallery photos select-photo]
+     (when selected-photo
+       [photo-viewer selected-photo close-viewer prev-photo next-photo])]))
+
+(rd/render [app]
            (. js/document (getElementById "app")))
 
 (defn on-js-reload []
