f1b9793a945bebced7ef5d7a2d91f26b7e84b55f
Paul Schulz <paul@mawsonlakes.org>
2026-06-02 09:41:04 +0930

    Update core.cljs to use File System Access API for local file loading

--- src/photo_archive/core.cljs
+++ src/photo_archive/core.cljs
@@ -2,16 +2,50 @@
     (:require
               [reagent.core :as reagent :refer [atom]]
               [reagent.dom :as rd]
-              [photo-archive.data :refer [sample-photos]]
+              [cljs.core.async :refer [go chan put! <! >!]]
+              [cljs.core.async.interop :refer-macros [<p!]]
+              [photo-archive.filesystem :as fs]
+              [photo-archive.manifest :as manifest]
               [photo-archive.components.gallery :refer [gallery]]
               [photo-archive.components.viewer :refer [photo-viewer]]))
 
 (enable-console-print!)
 
-;; define your app data so that it doesn't get over-written on reload
+;; App state
+(defonce app-state (atom {:photos []
+                          :selected-photo-id nil
+                          :dir-handle nil
+                          :loading false
+                          :error nil}))
 
-(defonce app-state (atom {:photos sample-photos
-                          :selected-photo-id nil}))
+;; Initialize directory and load photos
+(defn load-photos-from-directory []
+  (go
+    (try
+      (swap! app-state assoc :loading true :error nil)
+      
+      ;; Request directory access
+      (let [dir-handle (<p! (fs/request-directory-handle))]
+        (swap! app-state assoc :dir-handle dir-handle)
+        
+        ;; Scan for images
+        (let [images (<p! (fs/scan-images dir-handle))]
+          (if (empty? images)
+            (swap! app-state assoc :error "No images found in selected directory")
+            (do
+              ;; Create map of filename -> file handle
+              (let [file-handles (into {} (map (fn [img] [(:name img) (:handle img)]) images))
+                    ;; Load or create manifest
+                    manifest-data (<p! (manifest/load-or-create-manifest dir-handle images))]
+                
+                ;; Load photo data URLs
+                (let [photos-with-urls (<p! (manifest/load-photos-with-data-urls manifest-data file-handles))]
+                  (swap! app-state assoc :photos photos-with-urls
+                                        :loading false)))))))
+      
+      (catch :default e
+        (js/console.error "Error loading photos:" e)
+        (swap! app-state assoc :error (str "Error: " (.-message e)) :loading false)))))
 
 (defn get-photo-by-id [photos id]
   (first (filter #(= (:id %) id) photos)))
@@ -45,23 +79,37 @@
         prev-photo (nth photos prev-index)]
     (select-photo (:id prev-photo))))
 
+(defn folder-selector []
+  [:div.folder-selector
+   [:div.folder-selector-content
+    [:h2 "Photo Archive"]
+    [:p "Select a folder containing your photos"]
+    [:button.btn-primary {:on-click load-photos-from-directory}
+     "📁 Select Photo Folder"]
+    (when-let [error (:error @app-state)]
+      [:div.error-message error])
+    (when (:loading @app-state)
+      [:div.loading "Loading photos..."])]])
+
 (defn app []
   (let [photos (:photos @app-state)
         selected-id (:selected-photo-id @app-state)
         selected-photo (when selected-id
                         (get-photo-by-id photos selected-id))]
-    [:div.app
-     [:header.app-header
-      [:h1 "Photo Archive"]]
-     [gallery photos select-photo]
-     (when selected-photo
-       [photo-viewer selected-photo close-viewer prev-photo next-photo])]))
+    (if (empty? photos)
+      [folder-selector]
+      [:div.app
+       [:header.app-header
+        [:h1 "Photo Archive"]
+        [:button.btn-small {:on-click load-photos-from-directory}
+         "📁 Change Folder"]]
+       [gallery photos select-photo]
+       (when selected-photo
+         [photo-viewer selected-photo close-viewer prev-photo next-photo])])))
 
 (rd/render [app]
            (. js/document (getElementById "app")))
 
 (defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  ;; Reload hook for development
 )
